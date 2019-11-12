12173de5e98c7bdcdeca99aece160d2d35a9ab44
Robert <rmrowiec@icloud.com>
2019-11-12 16:22:55 +0100

    Added sort and select methods

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 const { defaultResponse, getFilter } = require('./common')
 
-exports.crud = function (collection, router) {
-	router.get('/', defaultResponse(200, () => collection.find().exec()))
+exports.crud = function (
+	collection,
+	router,
+	options = {}
+) {
+	if (!options || !options.methods) options.methods = ['Get', 'GetById', 'Pagination', 'Post', 'Put', 'Delete']
 
-				.get('/:id', defaultResponse(200, req => collection.findById(req.params.id).exec()))
+	console.log(options);
+	if (!options.sort) options.sort = '-createDate'
 
-				.get('/page/:page/limit/:limit', defaultResponse(200, req => {
-					const page = req.params.page;
-					const sort = req.query.sort ? req.query.sort.toString() : '-createDate'
-					const filter = getFilter(req.query)
-					const limit = +req.params.limit;
+	if (options) {
+		return options.methods.map(method => {
+	
+			switch (method) {
+				case 'Get':
+					return router.get('/', defaultResponse(200, () => collection.find().sort(options.sort)))
+				case 'GetById':
+					return router.get('/:id', defaultResponse(200, req => collection.findById(req.params.id)))
+				case 'Pagination':
+					return router.get('/page/:page/limit/:limit', defaultResponse(200, req => {
+						req.query.sort = options.sort
+						return pagination(req, collection)
+					}))
+				case 'Post':
+					return router.post('/', defaultResponse(201, req => new collection(req.body).save()))
+				case 'Put':
+					return router.put('/:id', defaultResponse(200, req => collection.findByIdAndUpdate(req.params.id, req.body, { new: true })))
+				case 'Delete':
+					return router.delete('/:id', defaultResponse(200, req => collection.findByIdAndRemove(req.params.id)))
+			}
+		})
+	}
+}
 
-					return Promise.all([
-						collection.countDocuments(filter).exec(),
-						collection.find(filter)
-							.sort(sort)
-							.skip(limit * (page - 1))
-							.limit(limit)
-							.exec()
-					]).then(([total, result]) => ({ total, result }))
-				}))
+const pagination = ( req, collection ) => {
+	const page = req.params.page;
+	const sort = req.query.sort.toString()
+	const filter = getFilter(req.query)
+	const limit = +req.params.limit;
 
-				.post('/', defaultResponse(201, req => new collection(req.body).save()))
-
-				.put('/:id', defaultResponse(200, req => collection.findByIdAndUpdate(req.params.id, req.body, { new: true }).exec()))
-
-				.delete('/:id', defaultResponse(200, req => collection.findByIdAndRemove(req.params.id).exec()))
+	return Promise.all([
+		collection.countDocuments(filter),
+		collection.find(filter)
+			.sort(sort)
+			.skip(limit * (page - 1))
+			.limit(limit)
+	]).then(([total, results]) => ({ 
+		total,
+		pages: Math.ceil(total / limit),
+		results,
+	}))
 }
